36031f15b4097e185c0dec7e641c6ed2486ce92c
Matthew Franglen <matthew@franglen.org>
2016-06-25 22:54:00 +0100

    Change brand

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,7 +20,7 @@ function Header() {
         <Navigation className={s.nav} />
         <Link className={s.brand} to="/">
           <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-          <span className={s.brandTxt}>Your Company</span>
+          <span className={s.brandTxt}>Maul Technology</span>
         </Link>
         <div className={s.banner}>
           <h1 className={s.bannerTitle}>React</h1>
