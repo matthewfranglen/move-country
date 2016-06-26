3fd59f978527a8bf76e2d1134a6db4788fd1bd10
Matthew Franglen <matthew@franglen.org>
2016-06-26 22:58:57 +0100

    Get europe overlay working
    The full set of countries is quite large

--- src/components/content/MapComponent.js
+++ src/components/content/MapComponent.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { GeoJson, Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import countryBorders from 'json!./europe.geo.json';
 
 require('styles/content/Map.scss');
 
@@ -10,11 +11,12 @@ const position = [51.505, -0.09];
 class MapComponent extends React.Component {
   render() {
     return (
-      <Map center={position} zoom={13}>
+      <Map center={position} zoom={3}>
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        <GeoJson data={countryBorders} />
         <Marker position={position}>
           <Popup>
             <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
