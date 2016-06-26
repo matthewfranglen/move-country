d37153af1ec34604bf7552009e8bbf81af1170e1
Matthew Franglen <matthew@franglen.org>
2016-06-26 13:10:58 +0100

    Still broken alt

--- src/components/content/MapComponent.js
+++ src/components/content/MapComponent.js
@@ -1,27 +1,35 @@
 'use strict';
 
 import React from 'react';
+import { render } from 'react-dom';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 require('styles/content/Map.scss');
 
 const position = [51.505, -0.09];
+const map = (
+  <Map center={position} zoom={13}>
+    <TileLayer
+      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    />
+    <Marker position={position}>
+      <Popup>
+        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+      </Popup>
+    </Marker>
+  </Map>
+);
 
 class MapComponent extends React.Component {
   render() {
     return (
-      <Map center={position} zoom={13}>
-        <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-          </Popup>
-        </Marker>
-      </Map>
+      <div id="map-container"></div>
     );
+  }
+
+  componentDidMount() {
+    render(map, document.getElementById('map-container'));
   }
 }
 
