6f6b6ac3673cd87b44819c60dbd86ee83a77ec40
Matthew Franglen <matthew@franglen.org>
2016-06-28 23:00:26 +0100

    Make country show name when clicked on

--- src/components/content/MapComponent.js
+++ src/components/content/MapComponent.js
@@ -1,50 +1,47 @@
 'use strict';
 
 import React from 'react';
-import { GeoJson, Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { GeoJson, Map, Marker, Popup, TileLayer, PropTypes as MapPropTypes } from 'react-leaflet';
 import countryBorders from 'json!./europe.geo.json';
 
 require('styles/content/Map.scss');
 
 const position = [51.505, -0.09];
 
+const style = {
+  color: '#333',
+  opacity: 0.3
+};
+
+const MyMarker = ({ children, position }) => (
+  <Marker position={position}>
+    <Popup>
+      <span>{children}</span>
+    </Popup>
+  </Marker>
+);
+MyMarker.propTypes = {
+  children: MapPropTypes.children,
+  position: MapPropTypes.latlng
+};
+
 class MapComponent extends React.Component {
   render() {
-    return (
+    this.map = (
       <Map center={position} zoom={3} maxZoom={5}>
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <GeoJson data={countryBorders} style={this.style} onEachFeature={this.events} />
-        <Marker position={position}>
-          <Popup>
-            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-          </Popup>
-        </Marker>
+        <GeoJson data={countryBorders} style={style} onEachFeature={this.events} />
       </Map>
     );
-  }
 
-  style (feature) {
-    if (feature.selected) {
-      return {
-        color: '#777',
-        opacity: 0.7
-      };
-    }
-    return {
-      color: '#333',
-      opacity: 0.3
-    };
+    return this.map;
   }
 
   events (feature, layer) {
-    feature.properties.selected = false;
-    layer.on('click', () => {
-      feature.properties.selected = !feature.properties.selected;
-      feature.setStyle(this.style);
-    });
+    layer.bindPopup(feature.properties.name);
   }
 }
 
