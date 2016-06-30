33d6894ecc06b7c0e0072482126faecec691801e
Matthew Franglen <matthew@franglen.org>
2016-06-30 23:58:53 +0100

    Move statistical functions into own file

--- src/components/content/StatisticsComponent.js
+++ src/components/content/StatisticsComponent.js
@@ -1,41 +1,7 @@
 'use strict';
 
 import React from 'react';
-
-require('styles/content/Statistics.scss');
-
-const statistics = [
-  {
-    key: 'HDI',
-    name: 'Human Development Index',
-    citation: 'http://hdr.undp.org',
-    // The ranks in the original document are 49 / 105 / 143. England is 14
-    ranks: {
-      high: 9,
-      medium: 18,
-      low: 36
-    }
-  },
-  {
-    key: 'PPI',
-    name: 'Property Price Index',
-    citation: 'http://www.numbeo.com/property-investment/region_rankings.jsp?title=2016&region=150',
-    // This was calculated by taking the average for the cities listed in the report.
-    // There are no ranks in the original. England is 12.2 London is 33.51 (the worst) and Glasgow is 4.80 (the best)
-    ranks: {
-      high: 8,
-      medium: 12,
-      low: 16
-    }
-  }
-];
-
-const emoji = {
-  high: '😎',
-  medium: '😊',
-  low: '😞',
-  very_low: '😧'
-};
+import { getTypes, getDescription, toEmoji } from './statistics';
 
 const StatisticsComponent = React.createClass({
   render: function() {
@@ -44,21 +10,21 @@ const StatisticsComponent = React.createClass({
     }
 
     var feature = this.props.feature.properties;
-    var makeResult = ({ name, value, citation }) => (
-      <div className="statistics__result">
-        <div className="statistics__label">{name}</div>
-        <div className="statistics__value">{value}</div>
-        <a href={citation} className="statistics__citation">[source]</a>
-      </div>
-    );
-    var results = statistics
-      .filter(s => s.key in feature)
-      .map(s => makeResult({
-            name: s.name,
-            value: this.toEmoji(feature[s.key], s.ranks),
-            citation: s.citation
-        })
+    var makeResult = (type) => {
+      var description = getDescription(type);
+      var value = toEmoji(feature[type], type);
+
+      return (
+        <div key={description.name} className="statistics__result">
+          <div className="statistics__label">{description.name}</div>
+          <div className="statistics__value">{value}</div>
+          <a href={description.citation} className="statistics__citation">[source]</a>
+        </div>
       );
+    };
+    var results = getTypes()
+      .filter(t => t in feature)
+      .map(makeResult);
 
     return (
       <div className="statistics">
@@ -70,19 +36,6 @@ const StatisticsComponent = React.createClass({
         </div>
       </div>
     );
-  },
-
-  toEmoji: function(value, ranks) {
-    if (value <= ranks.high) {
-      return emoji.high;
-    }
-    if (value <= ranks.medium) {
-      return emoji.medium;
-    }
-    if (value <= ranks.low) {
-      return emoji.low;
-    }
-    return emoji.very_low;
   }
 });
 
