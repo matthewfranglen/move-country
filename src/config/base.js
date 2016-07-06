7437bf06915a19c8e7c311b0ae6b789d0b2c7a31
Matthew Franglen <matthew@franglen.org>
2016-07-06 20:32:11 +0100

    Failed attempt to get fontawesome working

--- src/config/base.js
+++ src/config/base.js
@@ -2,4 +2,12 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
+  module: {
+    loaders: [
+      {
+        test: /\.eot/,
+        loader: 'file?name=[name].[ext]'
+      }
+    ]
+  }
 }
