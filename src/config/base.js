759d094be8373be23a00d9c916cf0d1721f8c2fa
Matthew Franglen <matthew@franglen.org>
2016-07-06 20:32:20 +0100

    Revert "Failed attempt to get fontawesome working"
    This reverts commit 7437bf06915a19c8e7c311b0ae6b789d0b2c7a31.

--- src/config/base.js
+++ src/config/base.js
@@ -2,12 +2,4 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
-  module: {
-    loaders: [
-      {
-        test: /\.eot/,
-        loader: 'file?name=[name].[ext]'
-      }
-    ]
-  }
 }
